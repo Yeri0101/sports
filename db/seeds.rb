bcfaa7c3e431afdbd6697acc4dd5d02c607e790a
Maximilien LANZ <maximilien.studio@icloud.com>
2022-11-29 16:35:47 +0100

    Add a gem and generate random content to feed the database seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+Activity.destroy_all
+
+5.times do
+  Activity.create!(
+    address: Faker::Address.street_address,
+    category: Faker::Sports.sport,
+    description: Faker::Lorem.sentence(word_count: 10, supplemental: true, random_words_to_add: 5),
+    end_date: Faker::Date.in_date_period(year: 2022, month: 11),
+    start_date: Faker::Date.between(from: '2021-11-29', to: '2022-11-29')
+  )
+end
+
+User.destroy_all
+
+5.times do
+  User.create!(
+    address: Faker::Address.street_address,
+    first_name: Faker::Name.first_name,
+    email: Faker::Internet.email(domain: 'sports'),
+    height: Faker::Number.number(digits: 3),
+    last_name: Faker::Name.last_name,
+    level: Faker::Lorem.words(number: 1, supplemental: false),
+    password: Faker::Internet.password(min_length: 6, max_length: 22),
+    phone_number: Faker::PhoneNumber.cell_phone_with_country_code,
+    weight: Faker::Number.number(digits: 3)
+  )
+end
