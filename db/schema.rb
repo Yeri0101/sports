ac5b42487e4483f84dbb80a16fc14ed95dae4318
Maximilien LANZ <maximilien.studio@icloud.com>
2022-11-29 17:13:03 +0100

    Create a new migration for the activities table and generate random content to feed the database seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_29_104727) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_29_160630) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -51,6 +51,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_29_104727) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "name"
     t.index ["user_id"], name: "index_activities_on_user_id"
   end
 
