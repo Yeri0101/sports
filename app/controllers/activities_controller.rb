2124ef915c59efb8cf80d6babac1f55843a9ab6a
yerinto martinez <yerinto.martinez@hotmail.fr>
2022-11-29 13:02:09 +0100

    Allow a user to create a new activity

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,2 +1,28 @@
 class ActivitiesController < ApplicationController
+  def index
+    @activities = Activity.all
+  end
+
+  def show
+  end
+
+  def new
+    @activity = Activity.new
+  end
+
+  def create
+    activity = Activity.new(activity_params)
+    activity.save
+    redirect_to activitys_path
+  end
+
+  private
+
+  def set_activity
+    @activity = activity.find(params[:id])
+  end
+
+  def activity_params
+    params.require(:activity).permit(:title, :details, :completed)
+  end
 end
