a4b0304716e9d00eef8572a58e63175c17ba862d
Maximilien LANZ <maximilien.studio@icloud.com>
2022-11-30 15:54:22 +0100

    Allow a user to create a new booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,32 @@
 class BookingsController < ApplicationController
+  def index
+    @bookings = current_user.bookings
+  end
+
+  def new
+    @booking = Booking.new
+    @activity = Activity.find(params[:activity_id])
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+
+    @booking.activity = @activity
+
+    if @booking.save
+      redirect_to activity_path(@activity), notice: "The booking has been made correctly"
+    else
+      render :new, status: :see_other
+    end
+  end
+
+  private
+
+  def set_booking
+    @activity = Activity.find(params[:id])
+  end
+
+  def booking_params
+    params.require(:booking).permit(%i[activity_id user_id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :activities do
+    resources :bookings
     resources :reviews, only: %i[create index new]
   end
 
