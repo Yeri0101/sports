8e133521ace45ca688e542344aeaffdbf4816774
Maximilien LANZ <maximilien.studio@icloud.com>
2022-12-01 10:56:16 +0100

    Allow a user to perform an extended search across existing activities

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,16 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :activities do
     resources :bookings, only: %i[create index new]
+
     resources :reviews, only: %i[create index new]
+
+    collection do
+      match 'search' => 'activities#search', via: %i[get post], as: :search
+    end
   end
 
   resources :bookings, only: %i[destroy edit show update]
+
   resources :reviews, only: %i[destroy edit show update]
   # Defines the root path route ("/")
   # root "articles#index"
