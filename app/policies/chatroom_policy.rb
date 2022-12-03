2b583d67fb687663c6b4eec0751e0c4c3bd73fbd
yerinto martinez <yerinto.martinez@hotmail.fr>
2022-12-03 18:48:05 +0100

    add controller messages for chat

---
+++ app/policies/chatroom_policy.rb
@@ -0,0 +1,24 @@
+class ChatroomPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      # scope.all
+      scope.where(user_id: user.id)
+    end
+  end
+
+  def show?
+    true
+  end
+
+  def create?
+    true
+  end
+
+  def update?
+    record.user == user
+  end
+
+  def destroy?
+    record.user == user
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
     resources :bookings, only: %i[create index new]
     resources :reviews, only: %i[create index new]
     resources :users
+    resources :chatrooms
 
     collection do
       match 'search' => 'activities#search', via: %i[get post], as: :search
@@ -14,9 +15,11 @@ Rails.application.routes.draw do
 
   resources :bookings, only: %i[destroy index edit show update]
   resources :reviews, only: %i[destroy edit show update]
-  resources :chatrooms, only: :show
+  resources :chatrooms, only: :show do
+    resources :messages, only: :create
 
   # Defines the root path route ("/")
   # root "articles#index"
   root to: "pages#home"
+  end
 end
