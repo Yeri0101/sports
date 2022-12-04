18916309875638b0fad3b9e67f9d59dca14cd2ff
yerinto martinez <yerinto.martinez@hotmail.fr>
2022-12-04 13:48:28 +0100

    add stimulus chatroom_subscription

---
+++ app/javascript/controllers/chatroom_subscription_controller.js
@@ -0,0 +1,12 @@
+// app/javascript/controllers/chatroom_subscription_controller.js
+import { Controller } from "@hotwired/stimulus"
+import { createConsumer } from "@rails/actioncable"
+
+export default class extends Controller {
+  static values = { chatroomId: Number }
+  static targets = ["messages"]
+
+  connect() {
+    console.log(`Subscribe to the chatroom with the id ${this.chatroomIdValue}.`)
+  }
+}
