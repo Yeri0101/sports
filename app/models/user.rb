ed98973714d3860b0519365bf1c4cde37bf9096f
Maximilien LANZ <maximilien.studio@icloud.com>
2022-12-06 09:06:24 +0100

    Fix major user interface issues

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,4 @@
 class User < ApplicationRecord
-  has_one_attached :avatar
-
-  has_many :activities, dependent: :destroy
-  has_many :bookings, dependent: :destroy
-
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
@@ -12,10 +7,12 @@ class User < ApplicationRecord
 
   has_many :activities, dependent: :destroy
   has_many :bookings, dependent: :destroy
+  has_many :reviews, dependent: :destroy
 
   validates :address, presence: true
   validates :city, presence: true
   validates :country, presence: true
+  validates :country_code, presence: true
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :phone_number, presence: true
