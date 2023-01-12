226e942444e3f5cf6e993d23762e74b53c871b8c
Maximilien LANZ <maximilien.studio@icloud.com>
2023-01-12 20:26:58 +0100

    Update of dependencies used in the project

---
+++ config/initializers/cloudinary.rb
@@ -0,0 +1,6 @@
+Cloudinary.config do |config|
+  config.cloud_name = "dxwptqse3"
+  config.api_key = "268915686244553"
+  config.api_secret = "jI-5D9N4VL6BowH6jrVWMcz5G5A"
+  config.secure = true
+end
