49cad879882a6e1d493dd90af8b46a20f7e48a47
Maximilien LANZ <maximilien.studio@icloud.com>
2023-01-26 13:52:49 +0100

    Update of dependencies used in the project

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,4 +2,6 @@ class Message < ApplicationRecord
   delegate :activity, to: :chatroom
   belongs_to :chatroom
   belongs_to :user
+  validates :message, presence: true
+  validates :message, length: { maximum: 1000, minimum: 10 }
 end
